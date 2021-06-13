a86030bed9581aac2ed5d90eb2fda0731afb6306
Prateek Reddy <prateekreddy274@gmail.com>
2021-06-13 16:13:37 +0530

    locally working

--- main.go
+++ main.go
@@ -9,14 +9,13 @@ import (
 	"danglingmind.com/ddd/infrastructure/persistence"
 	"danglingmind.com/ddd/interfaces"
 	"danglingmind.com/ddd/interfaces/middleware"
-	"github.com/joho/godotenv"
 	"github.com/sirupsen/logrus"
 )
 
 func main() {
 
 	// initialize configurations
-	godotenv.Load()
+	// godotenv.Load()
 	// initialize the log
 	logInstance := logrus.New()
 	logInstance.SetFormatter(&logrus.JSONFormatter{})
